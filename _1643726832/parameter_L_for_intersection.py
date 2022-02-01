7687307e564a5f52e1e263c27793d3429a717dce
Mike Burr <mb@unintuitive.org>
2022-02-01 10:37:42 -0700

    mul

--- _1643726832/parameter_L_for_intersection.py
+++ _1643726832/parameter_L_for_intersection.py
@@ -33,14 +33,51 @@ x**3 + a**x + b - y**2 --> {
 Let's try sum.
 """
 
+from itertools import product
+
+
 def add(p0, p1):
     results = {}
     common_terms = set(p0) & set(p1)
+    p0_ = p0.copy()
+    p1_ = p1.copy()
     for key in common_terms:
         results[key] = p0[key] + p1[key]
-        p0.pop(key)
-        p1.pop(key)
-    results.update(p0)
-    results.update(p1)
+        p0_.pop(key)
+        p1_.pop(key)
+    results.update(p0_)
+    results.update(p1_)
+    return {v: c for (v, c) in results.items() if c != 0}
+
+
+# for now... define just field element (e.g. 3.95/aka float) multiplication
+def mul(p0, r):
+    results = {}
+    for key, coeff in p0.items():
+        results[key] = coeff * r
     return results
-        
+
+
+# which permits us to define sub as
+def sub(p0, p1):
+    return add(p0, mul(p1, -1))
+
+
+def _get_symbol(sym, symbol_pows):
+    for symbol, power in symbol_pows:
+        if sym == symbol:
+            return sym, power
+
+
+def mul(p0, p1):
+    results = {}
+    for (vars0, coeff0), (vars1, coeff1) in product(p0.items(), p1.items()):
+        vars0 = dict(vars0)
+        vars1 = dict(vars1)
+        new_vars = []
+        for symbol in set(vars0) | set(vars1):
+            power = vars0.get(symbol, 0) + vars1.get(symbol, 0)
+            new_vars.append((symbol, power))
+
+        results[frozenset(new_vars)] = coeff0 * coeff1
+    return results

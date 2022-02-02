2f92bbf49d6af28cf35602567e7cef8910773bef
Mike Burr <mb@unintuitive.org>
2022-02-02 12:40:47 -0700

    au

--- _1643726832/parameter_L_for_intersection.e-boogleoo.py
+++ _1643726832/parameter_L_for_intersection.e-boogleoo.py
@@ -1,6 +1,5 @@
 
-# pip install git+https://github.com/stnbu/polynomier.git@d023fa334c93683db7db480b0350e0d81f7ece80
-
+# pip install git+https://github.com/stnbu/polynomier.git@19c9583
 # see p73 of https://www.amazon.com/Elliptic-Tales-Curves-Counting-Number/dp/0691163502/
 
 from polynomier import MultiPoly, fd
@@ -12,4 +11,14 @@ g_y = MultiPoly({fd({t: 1}): 1, fd(): -5})
 
 g = f.substitute(x, g_x).substitute(y, g_y)
 
-print(g)
+print("""
+f(x, y) = {f}
+x = {g_x}
+y = {g_y}
+f({g_x}, {g_y}) = {g}
+""".format(
+    f=f,
+    g_x=g_x,
+    g_y=g_y,
+    g=g,
+))

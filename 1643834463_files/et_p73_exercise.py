63bc348ae27698770e38187749d8c8fe4e325cdb
Mike Burr <mb@unintuitive.org>
2022-02-04 09:15:07 -0700

    .

--- 1643834463_files/et_p73_exercise.py
+++ 1643834463_files/et_p73_exercise.py
@@ -4,26 +4,16 @@ from sympy.plotting.plot import Plot, Parametric2DLineSeries, LineOver1DRangeSer
 from sympy.plotting.plot_implicit import *
 x, y, t = symbols("x y t")
 
+from sympy import *
+from sympy.plotting import plot_implicit
 
-f_series = ImplicitSeries(
-    Eq(x**2 + y + 4, 0),
-    var_start_end_x=(x, -5, 5),
-    var_start_end_y=(y, -8, 2),
-    has_equality=True,
-    use_interval_math=True,
-    depth=0,
-    nb_of_points=300,
-    line_color='blue')
+x, y = symbols("x y")
 
-plot_expr = [(2*t + 1, t - 5, (t, -10, 10))]
-l_series = [Parametric2DLineSeries(*arg) for arg in plot_expr]
+plot(Eq(x**2 + y + 4, 0), (2*t + 1, t - 5), backend="matplotlib")
 
-plot_expr = [(4*t**2 + 5*t, (t, -10, 10))]
-g_series = [LineOver1DRangeSeries(*arg) for arg in plot_expr]
+f_plot = plot_implicit(Eq(x**2 + y + 4, 0), xvar=(x, -5, 5), yvar=(y, -8, 2), show=False, backend="matplotlib")
+l_plot = plot_parametric((2*t + 1, t - 5), xlim=(-2.5, 2.5), ylim=(-8, -2), show=False, backend="matplotlib")
+l_plot.extend(f_plot)
 
-p = Plot(f_series, *l_series, *g_series, xlim=(-5, 5), ylim=(-8, 2), labels=True)
-
-from btools import *
-#import ipdb; ipdb.set_trace()
-write_to_file(p, __file__ + ".svg")
-#print('done')
+l_plot.show()
+#import btools ; btools.write_svg_to_file(plot, __file__ + ".svg")

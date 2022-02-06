b8c3d315c363da0d3d217d1811007babd5f24e9a
Mike Burr <mb@unintuitive.org>
2022-02-06 11:23:53 -0700

    .

--- 1643834463_files/curve-line_intersect_negL.py
+++ 1643834463_files/curve-line_intersect_negL.py
@@ -1,15 +1,9 @@
-# pip install git+https://github.com/stnbu/polynomier.git@8f7bdb5
-# see p73 of https://www.amazon.com/Elliptic-Tales-Curves-Counting-Number/dp/0691163502/
-
-from polynomier import MultiPoly, fd
+from polynomier import Polynomial, fd
 from polynomier.symbols import *
 
-f = MultiPoly({fd({x: 2}): 1, fd({y: 1}): 1, fd(): 4})
-g_x = MultiPoly({fd({t: 1}): 2, fd(): 1}) * -1
-g_y = MultiPoly({fd({t: 1}): 1, fd(): -5})
-
-# swap the x and y parameterization expressions
-# g_x, g_y = g_y, g_x
+f = Polynomial({fd({x: 2}): 1, fd({y: 1}): 1, fd(): 4})
+g_x = Polynomial({fd({t: 1}): 2, fd(): 1}) * -1
+g_y = Polynomial({fd({t: 1}): 1, fd(): -5})
 
 g = f.substitute((x, g_x), (y, g_y))
 
--- 1643834463_files/curve-line_nointersect.py
+++ 1643834463_files/curve-line_nointersect.py
@@ -1,12 +1,9 @@
-# pip install git+https://github.com/stnbu/polynomier.git@8f7bdb5
-# see p73 of https://www.amazon.com/Elliptic-Tales-Curves-Counting-Number/dp/0691163502/
-
-from polynomier import MultiPoly, fd
+from polynomier import Polynomial, fd
 from polynomier.symbols import *
 
-f = MultiPoly({fd({x: 2}): 1, fd({y: 1}): 1, fd(): 4})
-g_x = MultiPoly({fd({t: 1}): 1, fd(): 3})
-g_y = MultiPoly({fd({t: 1}): 2, fd(): 8})
+f = Polynomial({fd({x: 2}): 1, fd({y: 1}): 1, fd(): 4})
+g_x = Polynomial({fd({t: 1}): 1, fd(): 3})
+g_y = Polynomial({fd({t: 1}): 2, fd(): 8})
 
 g = f.substitute((x, g_x), (y, g_y))
 
